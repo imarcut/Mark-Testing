d55c09549ffa66d675965eedde39189127388670
unknown <marcut.ionut@gmail.com>
2022-03-07 18:41:09 +0200

    fixed Ui test

--- generate-report.js
+++ generate-report.js
@@ -6,15 +6,7 @@ var options = {
     output: 'cypress/test-artifacts/html-reporter/cucumber-report.html',
     reportSuiteAsScenarios: true,
     scenarioTimestamp: true,
-    launchReport: false,
-    metadata: {
-       "App Version":"0.3.2",
-       "Test Environment": "STAGING",
-       "Browser": "Chrome  54.0.2840.98",
-       "Platform": "Windows 10",
-       "Parallel": "Scenarios",
-       "Executed": "Remote"
-    }
-};
+    launchReport: false
+}
 
 reporter.generate(options);
